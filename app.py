5d14972c81b64cdef5964330d3b7767ddd0dab98
anuragpathak27 <pathakanurag445@gmail.com>
2025-04-29 16:33:40 +0530

    Updated requirements

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import joblib
+# import joblib
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score
